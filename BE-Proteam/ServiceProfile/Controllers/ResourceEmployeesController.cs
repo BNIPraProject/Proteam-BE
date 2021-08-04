81d77b3f47fc57acae5c6842900885cc9a101ddb
malwanw <muhammadalwan.52@gmail.com>
2021-08-05 00:35:54 +0700

    Add resource employee

---
+++ BE-Proteam/ServiceProfile/Controllers/ResourceEmployeesController.cs
@@ -0,0 +1,231 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using ServiceProfile.Component;
+using ServiceProfile.Data;
+using ServiceProfile.Models;
+using ServiceProfile.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ServiceProfile.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResourceEmployeesController : ControllerBase
+    {
+        private IResourceEmployee _resourceemployee;
+        public ProteamContext _context;
+
+        public ResourceEmployeesController(IResourceEmployee resourceemployee, ProteamContext context)
+        {
+            _context = context;
+            _resourceemployee = resourceemployee;
+        }
+      
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+
+            try
+            {
+                List<Vm> data = new List<Vm>();
+                List<Vm> employee = new List<Vm>();
+                List <SkillName> skillname = new List<SkillName>();
+
+
+                data = StoredProcedureExecutor.ExecuteSPList<Vm>(_context, "join_skillset", new SqlParameter[]{
+                        new SqlParameter("@Flag", 1),
+                        new SqlParameter("@Parameter1", "Test"),
+                        new SqlParameter("@Parameter2", "Test")
+                });
+
+
+                foreach (Vm Data2 in data)
+                {
+                    Vm NewData = new Vm();
+                    NewData.EmployeeId = Data2.EmployeeId;
+                    NewData.EmployeeName = Data2.EmployeeName;
+                    NewData.Npp = Data2.Npp;
+                    NewData.Email = Data2.Email;
+                    NewData.Phone = Data2.Phone;
+                    NewData.ActiveDate = Data2.ActiveDate;
+                    NewData.LastWorkDate = Data2.LastWorkDate;
+                    NewData.TotalManhour = Data2.TotalManhour;
+                    NewData.tipe_resource = Data2.tipe_resource;
+                    NewData.nama_role = Data2.nama_role;
+                    NewData.Kelompok = Data2.Kelompok;
+                    NewData.JenjangJabatan = Data2.JenjangJabatan;
+                    NewData.ProjectExp = Data2.ProjectExp;
+                    NewData.Status = Data2.Status;
+                    NewData.CreatedBy = Data2.CreatedBy;
+                    NewData.UpdatedBy = Data2.UpdatedBy;
+                    NewData.CreatedTime = Data2.CreatedTime;
+                    NewData.UpdateTime = Data2.UpdateTime;
+                    NewData.ResourceType = Data2.ResourceType;
+                    NewData.Role = Data2.Role;
+                    NewData.JenjabId = Data2.JenjabId;
+                    NewData.KelompokId = Data2.KelompokId;
+                    NewData.nama_status = Data2.nama_status;
+
+                    skillname = StoredProcedureExecutor.ExecuteSPList<SkillName>(_context, "join_skillset", new SqlParameter[]{
+                        new SqlParameter("@Flag", 2),
+                        new SqlParameter("@Parameter1", NewData.EmployeeId),
+                        new SqlParameter("@Parameter2", "Test")
+                    });
+                    if (skillname.Count() > 0)
+                    {
+                        string DataSemuaRole = "";
+                        foreach (SkillName role in skillname)
+                        {
+                            DataSemuaRole = DataSemuaRole + " " + role.Skillset + "</b> <br/>";
+                        }
+                        NewData.Skill = DataSemuaRole;
+                    }
+                    employee.Add(NewData);
+                    
+                }
+
+
+                return Ok(new { employee });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error : " + ex.Message);
+            }
+            var results = await _resourceemployee.GetAll();
+
+        }
+
+
+        // GET api/<ResourceEmployeesController>/5
+        //    [HttpGet("{id}")]
+        //public async Task<ResourceEmployee> Get(int id)
+        //{
+        //    var result = await _resourceemployee.GetById(id.ToString());
+        //    return result;
+        //}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+
+            try
+            {
+                List<Vm> data = new List<Vm>();
+                List<Vm> employee = new List<Vm>();
+                List<SkillName> skillname = new List<SkillName>();
+
+
+                data = StoredProcedureExecutor.ExecuteSPList<Vm>(_context, "join_skillset_byid", new SqlParameter[]{
+                        new SqlParameter("@Flag", 1),
+                        new SqlParameter("@Parameter1", id),
+                        new SqlParameter("@Parameter2", "Test")
+                });
+
+
+                foreach (Vm Data2 in data)
+                {
+                    Vm NewData = new Vm();
+                    NewData.EmployeeId = Data2.EmployeeId;
+                    NewData.EmployeeName = Data2.EmployeeName;
+                    NewData.Npp = Data2.Npp;
+                    NewData.Email = Data2.Email;
+                    NewData.Phone = Data2.Phone;
+                    NewData.ActiveDate = Data2.ActiveDate;
+                    NewData.LastWorkDate = Data2.LastWorkDate;
+                    NewData.TotalManhour = Data2.TotalManhour;
+                    NewData.tipe_resource = Data2.tipe_resource;
+                    NewData.nama_role = Data2.nama_role;
+                    NewData.Kelompok = Data2.Kelompok;
+                    NewData.JenjangJabatan = Data2.JenjangJabatan;
+                    NewData.ProjectExp = Data2.ProjectExp;
+                    NewData.Status = Data2.Status;
+                    NewData.CreatedBy = Data2.CreatedBy;
+                    NewData.UpdatedBy = Data2.UpdatedBy;
+                    NewData.CreatedTime = Data2.CreatedTime;
+                    NewData.UpdateTime = Data2.UpdateTime;
+                    NewData.ResourceType = Data2.ResourceType;
+                    NewData.Role = Data2.Role;
+                    NewData.JenjabId = Data2.JenjabId;
+                    NewData.KelompokId = Data2.KelompokId;
+                    NewData.nama_status = Data2.nama_status;
+
+                    skillname = StoredProcedureExecutor.ExecuteSPList<SkillName>(_context, "join_skillset", new SqlParameter[]{
+                        new SqlParameter("@Flag", 2),
+                        new SqlParameter("@Parameter1", id),
+                        new SqlParameter("@Parameter2", "Test")
+                    });
+                    if (skillname.Count() > 0)
+                    {
+                        string DataSemuaRole = "";
+                        foreach (SkillName role in skillname)
+                        {
+                            DataSemuaRole = DataSemuaRole + " " + role.Skillset + "</b> <br/>";
+                        }
+                        NewData.Skill = DataSemuaRole;
+                    }
+                    employee.Add(NewData);
+
+                }
+
+
+                return Ok(new { employee });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error : " + ex.Message);
+            }
+            var results = await _resourceemployee.GetAll();
+
+        }
+
+
+        // POST api/<ResourceEmployeesController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] ResourceEmployee resourceemployee)
+        {
+            try
+            {
+                await _resourceemployee.Insert(resourceemployee);
+                return Ok($"Data Employee {resourceemployee.EmployeeName} berhasil ditambahkan");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/<ResourceEmployeesController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] ResourceEmployee resourceemployee)
+        {
+            try
+            {
+                await _resourceemployee.Update(id.ToString(), resourceemployee);
+                return Ok($"Data Employee ID {id} berhasil diupdate");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<ResourceEmployeesController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _resourceemployee.Delete(id.ToString());
+                return Ok($"Data Employee {id} berhasil dihapus");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ BE-Proteam/ServiceProfile/Data/ResourceEmployeeData.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceProfile.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using ServiceProfile.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MasterRole_GetById.Tools;
+
+namespace ServiceProfile.Data
+{
+    public class ResourceEmployeeData : IResourceEmployee
+    {
+        private ProteamContext _db;
+
+        public ResourceEmployeeData(ProteamContext db)
+        {
+            _db = db;
+        }
+        public async Task Delete(string id)
+        {
+            var result = await GetById(id);
+            //cek apakah data ada?
+            if (result != null)
+            {
+                try
+                {
+                    _db.ResourceEmployees.Remove(result);
+                    await _db.SaveChangesAsync();
+                }
+                catch (DbUpdateException dbEx)
+                {
+                    throw new Exception($"DbError: {dbEx.Message}");
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Error: {ex.Message}");
+                }
+            }
+        }
+        //#region LoadDataInboxRM
+       // [Authorize]
+       // [HttpPost]
+        //public async Task<List<Vm>> GetDataInboxRM()
+        //{
+
+        //    List<Vm> res = new List<Vm>();
+        //    try
+        //    {
+        //        List<Vm> list = new List<Vm>();
+              
+
+        //        list = StoredProcedureExecutor.ExecuteSPList<Vm>(_db, "sp_data", new SqlParameter[]{
+        //               });
+
+              
+
+               
+        //        return list;
+        //    }
+        //    catch (Exception ex)
+        //    {
+               
+        //        return res;
+        //    }
+        //}
+       // #endregion
+        public async Task<IEnumerable<ResourceEmployee>> GetAll()
+        {
+
+             var results = await (from re in _db.ResourceEmployees.Include(e => e.Jenjab).Include(e => e.Kelompok)
+                                 select re).ToListAsync();
+            // var employeeskills = await (from e in _db.EmployeeSkills.Include(e => e.Skillset)
+            //                            select e).AsNoTracking().ToListAsync();
+            // for(int i=0; i < results.Count(); i++)
+            // {
+            //     results[i].EmployeeSkills = employeeskills;
+            //var countsql = "exec SP_Select_Count_DynamicKendoGrid "
+            //                     + "@KendoGrid='" + filter + "'";
+            //int totalRow = _db.Database.SqlQuery<int>(countsql).First();
+
+            //var xxxa = await _db.ResourceEmployees.FromSqlRaw("SELECT * FROM ResourceEmployee").ToListAsync();
+
+            //   var xxxa = await _db.ResourceEmployees.FromSqlRaw("SELECT * FROM ResourceEmployee").ToListAsync();
+            //  //var result = await context.SomeModels.FromSql("SQL_SCRIPT").ToListAsync();
+
+            //  List<Vm> Data34 = xxxa.ToList();
+
+            //}234
+
+            return results;
+        }
+
+        public async Task<ResourceEmployee> GetById(string id)
+        {
+            var result = await(from re in _db.ResourceEmployees.Include(e => e.Jenjab).Include(e => e.Kelompok)
+                               where re.EmployeeId == Convert.ToInt32(id)
+                               select re).FirstOrDefaultAsync();
+
+            var employeeskills = await (from e in _db.EmployeeSkills.Include(e => e.Skillset)
+                                     where e.EmployeeId == Convert.ToInt32(id)
+                                     select e).AsNoTracking().ToListAsync();
+            result.EmployeeSkills = employeeskills;
+
+            return result;
+        }
+
+        public async Task Insert(ResourceEmployee obj)
+        {
+               try
+            {
+                _db.ResourceEmployees.Add(obj);
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateException dbEx)
+            {
+                throw new Exception($"Db Error: {dbEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error: {ex.Message}");
+            }
+        }
+
+        public async Task Update(string id, ResourceEmployee obj)
+        {
+            try
+            {
+                var result = await GetById(id);
+                //cek apakah data sudah ada
+                if (result != null)
+                {
+                    //_db.Update(obj);
+                    result.EmployeeName = obj.EmployeeName;
+                    result.Email = obj.Email;
+                    result.Phone = obj.Phone;
+                    result.ActiveDate = obj.ActiveDate;
+                    result.LastWorkDate = obj.LastWorkDate;
+                    result.TotalManhour = obj.TotalManhour;
+                    result.ResourceType = obj.ResourceType;
+                    result.JenjabId = obj.JenjabId;
+                    result.KelompokId = obj.KelompokId;
+                    result.Role = obj.Role;
+                    result.Status = obj.Status;
+                    result.UpdatedBy = obj.UpdatedBy;
+                    result.UpdateTime = obj.UpdateTime;
+                    await _db.SaveChangesAsync();
+                }
+                else
+                {
+                    throw new Exception($"Data id:{id} tidak ditemukan");
+                }
+            }
+            catch (DbUpdateException dbEx)
+            {
+                throw new Exception($"DbError: {dbEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error: {ex.Message}");
+            }
+        }
+    }
+}
